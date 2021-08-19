137eea547908cca4c537e5812e53137d443e21cc
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-08-19 20:27:38 +0600

    added a gif.

---
+++ src/BackwardsReadPrimes.java
@@ -0,0 +1,246 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class BackwardsReadPrimes {
+    //    public class Main {
+
+    static boolean isPrime(long n) {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (long i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        return true;
+    }
+
+    public static long reverse(long n) {
+        long rev = 0;
+        while (n != 0) {
+            rev = rev * 10;
+            rev = rev + n % 10;
+            n = n / 10;
+        }
+        return rev;
+    }
+
+    public static String backwardsPrime(long start, long end) {
+
+        StringBuilder sb = new StringBuilder();
+        for (long i = start; i <= end; i++) {
+            long rev=reverse(i);
+            if (rev!=i && isPrime(i) && isPrime(rev)) {
+                sb.append(i).append(' ');
+            }
+        }
+        if(sb.length()==0){
+            return "";
+        }
+
+        return sb.substring(0,sb.length()-1).toString();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        out.println(backwardsPrime(1,100)+"");
+
+        close.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* This is O(scary), but seems quick enough in practice. */
+
+    static void debug(Object... obj) {
+        System.err.println(Arrays.deepToString(obj).replace("], ", "]\n"));
+    }
+
+    static void debugV2(Object... obj) {
+        System.out.println(Arrays.deepToString(obj)
+                .replace("],", "\n").replace(",", "\t")
+                .replaceAll("[\\[\\]]", " "));
+    }
+
+    static class Sc {
+        final private int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Sc() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Sc(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public String nextString() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int n = read();
+            while (isWhiteSpace(n))
+                n = read();
+            while (!isWhiteSpace(n)) {
+                sb.append((char) n);
+                n = read();
+            }
+            return sb.toString();
+        }
+
+        private boolean isWhiteSpace(int n) {
+            return n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
+        }
+    }
+
+
+    static class Print {
+        private final OutputStream out;
+
+        public Print() {
+            this.out = System.out;
+        }
+
+        public void print(String str) throws IOException {
+            for (int i = 0; i < str.length(); i++) {
+                out.write(str.charAt(i));
+            }
+        }
+
+        public void println(String str) throws IOException {
+            print(str);
+            out.write('\n');
+        }
+
+        public void close() throws IOException {
+            out.close();
+        }
+    }
+
+    static Sc sc;
+
+    static {
+        try {
+            sc = new Sc("test.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Print out = new Print();
+    static BufferedWriter close = new BufferedWriter(new OutputStreamWriter(System.out));
+    // 8:07 PM Thu 08 2021 19/08/2021
+}
